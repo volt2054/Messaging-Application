5fc1d0a4f8438cb51a618a58f64ffab00214869d
Ethan Williamson <ethan.williamson.2006@gmail.com>
2023-05-09 12:28:23 +0100

    Added ability to create a user

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -13,13 +13,47 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Net.Sockets;
+using System.Net;
+
 namespace Client {
+
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        private static TcpClient client;
+
+
         public MainWindow() {
             InitializeComponent();
+
+            try {
+                IPAddress ipAddress = IPAddress.Parse("127.0.0.1"); // Connect to local host
+                int port = 7256;
+                client = new TcpClient(ipAddress.ToString(), port);
+                MessageBox.Show($"Connected to server on {ipAddress}:{port}");
+
+                NetworkStream stream = client.GetStream();
+                string message = "CREATE testttusername testemailll testpasswordd";
+                byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+                stream.Write(messageBytes, 0, messageBytes.Length);
+                MessageBox.Show($"Sent message: {message}");
+
+                byte[] responseBytes = new byte[1024];
+                int bytesRead = stream.Read(responseBytes, 0, responseBytes.Length);
+                string responseMessage = Encoding.ASCII.GetString(responseBytes, 0, bytesRead);
+                MessageBox.Show($"Recieved response {responseMessage}");
+            } catch (Exception ex) {
+                MessageBox.Show($"An Error Occured: {ex.Message}");
+            } finally {
+                client.Close();
+                MessageBox.Show("Connection closed");
+            }
+
+
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -3,7 +3,11 @@ using System.Collections.Immutable;
 using System.Data;
 using System.Runtime.CompilerServices;
 using Microsoft.Data.SqlClient;
-
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Text;
+using System.Diagnostics;
 
 namespace Server {
 
@@ -15,14 +19,76 @@ namespace Server {
             DropTables();
             Console.WriteLine("Creating Tables");
             CreateTables();
-            Console.WriteLine("Inserting user");
-            InsertNewUser("testuser123", "testemail123", "testpassword123");
-            SelectAll();
+
+            bool isRunning = true;
+
+            TcpListener listener;
+            StartServer(out listener);
+
+            while (isRunning) {
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Client connected from {0}", client.Client.RemoteEndPoint);
+
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClient));
+                clientThread.Start(client);
+
+            }
 
             Console.ReadLine();
         }
 
-        static void InsertNewUser(string username, string email, string password) {
+        
+        private static void HandleClient(object obj) {
+            TcpClient client = (TcpClient)obj;
+
+            try {
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Recieved message from {0}: {1}", client.Client.RemoteEndPoint, message);
+
+                string responseMessage = "";
+                string[] args = message.Split(" ");
+
+
+                if (message.StartsWith("SEND")) {           // SEND MESSAGES
+
+                } else if (message.StartsWith("CREATE")) {  // CREATE USER
+                    string username = args[1];
+                    string email = args[2];
+                    string password = args[3];
+                    responseMessage = InsertNewUser(username, email, password);
+                    SelectAll();
+                } else if (message.StartsWith("DELETE")) {  // DELETE MESSAGES
+
+                } else if (message.StartsWith("GET")) {     // FETCH MESSAGES
+
+                } else {
+
+                }
+
+                byte[] responseBytes = Encoding.ASCII.GetBytes(responseMessage);
+                stream.Write(responseBytes, 0, responseBytes.Length);
+            } catch (Exception ex) {
+                Console.WriteLine("Error handling client: {0}", ex.Message);
+            } finally {
+                client.Close();
+                Console.WriteLine("Client disconnected");
+            }
+        }
+
+        private static void StartServer(out TcpListener listener) {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            int port = 7256;
+            listener = new TcpListener(ipAddress, port);
+            listener.Start();
+            Console.WriteLine("Server started");
+        }
+
+        static void DeleteUser(string username) { }
+
+        static string InsertNewUser(string username, string email, string password) {
             ExecuteDatabaseOperations(connection => {
                 string insertQuery = "INSERT INTO Users (username, email, password) VALUES(@value1, @value2, @value3)";
                 using (SqlCommand command = new SqlCommand(insertQuery, connection)) {
@@ -31,11 +97,21 @@ namespace Server {
                     command.Parameters.AddWithValue("@value2", email);
                     command.Parameters.AddWithValue("@value3", password);
 
-
                     command.ExecuteNonQuery();
                 }
             });
+
+            List<string> result = new List<string>();
+
+
+            ExecuteDatabaseOperations(connection => {
+                string selectQuery = $"SELECT user_id FROM Users WHERE username = '{username}'";
+                result = ExecuteQuery(connection, selectQuery);
+            });
+
+            return result.Last();
         }
+
 
         static void SelectAll() {
             List<string> result = new List<string>();
@@ -50,31 +126,6 @@ namespace Server {
             }
         }
 
-        static void TestInsertAndSelect() {
-            ExecuteDatabaseOperations(connection => {
-                string insertQuery = "INSERT INTO Users (username, email, password) VALUES(@value1, @value2, @value3)";
-                using (SqlCommand command = new SqlCommand(insertQuery, connection)) {
-                    connection.Open();
-                    command.Parameters.AddWithValue("@value1", "TestUser");
-                    command.Parameters.AddWithValue("@value2", "TestEmail");
-                    command.Parameters.AddWithValue("@value3", "TestPass");
-                    command.ExecuteNonQuery();
-                }
-            });
-
-            List<string> result = new List<string>();
-
-            ExecuteDatabaseOperations(connection => {
-                string selectQuery = "SELECT * FROM Users";
-                result = ExecuteQuery(connection, selectQuery);
-            });
-
-            foreach (string row in result) {
-                Console.WriteLine(row);
-            }
-
-        }
-
         static void TestDatabaseConnection() {
             try {
                 ExecuteDatabaseOperations(connection => {
@@ -87,7 +138,7 @@ namespace Server {
         }
 
         static void ExecuteDatabaseOperations(Action<SqlConnection> databaseOperation) {
-            string databaseFilePath = @"C:\Users\ethan\Documents\dev\Server\database.mdf";
+            string databaseFilePath = @"C:\Users\school work\Documents\dev\Messaging-Application\Server\Database.mdf";
             string connectionString = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={databaseFilePath};Integrated Security=True";
 
             using (SqlConnection connection = new SqlConnection(connectionString)) {
