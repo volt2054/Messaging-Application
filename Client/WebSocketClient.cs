f7dbc14106ca9cd48f182fbccd211bd8cb109b25
volt2054 <ethan.williamson.2006@gmail.com>
2023-09-08 09:25:20 +0100

    Revert "Wrapped WebsocketClient classes and implemented methods for a shared client ID"
    This reverts commit 24b59d10d5a4ae2831c83148a32212f4aed27277.

--- Client/WebSocketClient.cs
+++ Client/WebSocketClient.cs
@@ -7,11 +7,6 @@ using System.Net.WebSockets;
 using System.Threading;
 using System.Windows;
 
-using static SharedLibrary.WebSocket;
-using System.Security.Cryptography.X509Certificates;
-using SharedLibrary;
-using System.Windows.Media.Animation;
-
 
 
 // TODO SPLIT INTO MESSAGE HANDLER AND COMMUNICATION HANDLER
@@ -21,7 +16,23 @@ using System.Windows.Media.Animation;
 namespace Client {
     class WebSocketClient {
 
-        internal static string ClientID;
+        const string DELIMITER = "|< delimiter >|"; //TODO replace with something else
+        //const int PORT = 7256;
+        //const string IP_ADDRESS = "127.0.0.1";
+
+        private static readonly string SERVER_URL = $"ws://127.0.0.1:7256";
+        private static ClientWebSocket _webSocket;
+
+        private static string clientID = "";
+
+
+        public static async Task ConnectWebSocket() {
+            _webSocket = new ClientWebSocket();
+            Uri serverUri = new Uri(SERVER_URL);
+            await _webSocket.ConnectAsync(serverUri, CancellationToken.None);
+
+            clientID = await ReceiveClientID(_webSocket);
+        }
 
         private static async Task<string> ReceiveClientID(ClientWebSocket webSocket) {
             byte[] buffer = new byte[1024];
@@ -30,95 +41,58 @@ namespace Client {
             return clientId;
         }
 
-        private static async void ConnectWebsocket(ClientWebSocket _webSocket) {
-            _webSocket = new ClientWebSocket();
-            Uri serverUri = new Uri(SERVER_URL);
-            await _webSocket.ConnectAsync(serverUri, CancellationToken.None);
-            ClientID = await ReceiveClientID(_webSocket);
+        public static async Task<string> RecieveMessage() {
+            byte[] buffer = new byte[1024];
+            WebSocketReceiveResult result = await _webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            string message = Encoding.ASCII.GetString(buffer, 0, result.Count);
+            MessageBox.Show("RECIEVE");
+            return message;
         }
 
-        WebSocketClient() {
-            ClientWebSocket _webSocket = new ClientWebSocket();
-
-            ConnectWebsocket(_webSocket);
-        }
-
-        class MessageHandler {
-
-        }
-
-        class ServerCommunication {
-
-            private static ClientWebSocket _webSocket;
-
-            public static async Task ConnectWebSocket() {
-                _webSocket = new ClientWebSocket();
-                Uri serverUri = new Uri(SERVER_URL);
-                await _webSocket.ConnectAsync(serverUri, CancellationToken.None);
-
-                ClientID = await ReceiveClientID(_webSocket);
-            }
-
-            private static async Task<string> ReceiveClientID(ClientWebSocket webSocket) {
-                byte[] buffer = new byte[1024];
-                WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                string clientId = Encoding.ASCII.GetString(buffer, 0, result.Count);
-                return clientId;
-            }
-
-            public static async Task<string> RecieveMessage() {
-                byte[] buffer = new byte[1024];
-                WebSocketReceiveResult result = await _webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                string message = Encoding.ASCII.GetString(buffer, 0, result.Count);
-                MessageBox.Show("RECIEVE");
-                return message;
-            }
-
-            public static async Task CloseWebSocket() {
-                try {
-                    if (_webSocket != null && _webSocket.State == WebSocketState.Open) {
-                        await _webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Client closed", CancellationToken.None);
-                    } else {
-                        MessageBox.Show("No connection to close");
-                    }
-                } catch (Exception ex) {
-                    MessageBox.Show($"Error during WebSocket closure: {ex.Message}");
+        public static async Task CloseWebSocket() {
+            try {
+                if (_webSocket != null && _webSocket.State == WebSocketState.Open) {
+                    await _webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Client closed", CancellationToken.None);
+                } else {
+                    MessageBox.Show("No connection to close");
                 }
+            } catch (Exception ex) {
+                MessageBox.Show($"Error during WebSocket closure: {ex.Message}");
             }
+        }
 
-            public static async Task<string> CreateCommunication(string communicationType, string[] data) {
-                string responseMessage = "-1"; // FAILED
+        public static async Task<string> CreateCommunication(string communicationType, string[] data) {
+            string responseMessage = "-1"; // FAILED
 
-                // Generate a UID for the request
-                string requestId = Guid.NewGuid().ToString();
+            // Generate a UID for the request
+            string requestId = Guid.NewGuid().ToString();
 
-                try {
-                    string message = $"{requestId}:{ClientID}{DELIMITER}{communicationType}";
-                    foreach (string datum in data) {
-                        message += DELIMITER;
-                        message += datum;
-                    }
-
-                    byte[] messageBytes = Encoding.ASCII.GetBytes(message);
-                    await _webSocket.SendAsync(new ArraySegment<byte>(messageBytes), WebSocketMessageType.Text, true, CancellationToken.None);
-
-                    byte[] responseBytes = new byte[1024];
-                    WebSocketReceiveResult receiveResult = await _webSocket.ReceiveAsync(new ArraySegment<byte>(responseBytes), CancellationToken.None);
-                    responseMessage = Encoding.ASCII.GetString(responseBytes, 0, receiveResult.Count);
-
-                    //TODO SERVER ADD REQUEST ID
-                    //responseMessage = responseMessage.Substring(requestId.Length + 1);
-
-
-                    MessageBox.Show("CREATECOMM");
-                } catch (Exception ex) {
-                    MessageBox.Show($"Error Occurred Creating WebSocket Communication: {ex.Message}");
+            try {
+                string message = $"{requestId}:{clientID}{DELIMITER}{communicationType}";
+                foreach (string datum in data) {
+                    message += DELIMITER;
+                    message += datum;
                 }
-                return responseMessage;
 
+                byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+                await _webSocket.SendAsync(new ArraySegment<byte>(messageBytes), WebSocketMessageType.Text, true, CancellationToken.None);
+
+                byte[] responseBytes = new byte[1024];
+                WebSocketReceiveResult receiveResult = await _webSocket.ReceiveAsync(new ArraySegment<byte>(responseBytes), CancellationToken.None);
+                responseMessage = Encoding.ASCII.GetString(responseBytes, 0, receiveResult.Count);
+
+                //TODO SERVER ADD REQUEST ID
+                //responseMessage = responseMessage.Substring(requestId.Length + 1);
+
+
+                MessageBox.Show("CREATECOMM");
+            } catch (Exception ex) {
+                MessageBox.Show($"Error Occurred Creating WebSocket Communication: {ex.Message}");
             }
+            return responseMessage;
 
         }
+
 
     }
 }
