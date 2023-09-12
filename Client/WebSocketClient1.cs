58636421d879c7132a4ff2699cf26afa9c2c3430
volt2054 <ethan.williamson.2006@gmail.com>
2023-09-12 10:51:06 +0100

    Started creating reworked WebSocketClient class which handles messages in a Queue format which allows for easy splitting of responses and messages

--- Client/WebSocketClient1.cs
+++ Client/WebSocketClient1.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Net.WebSockets;
 using System.Threading;
 using System.Windows;
+using static SharedLibrary.WebSocket;
 using SharedLibrary;
 
 
@@ -16,7 +17,101 @@ using SharedLibrary;
 
 
 namespace Client {
+
     class WebSocketClient {
+
+        Queue<string> responseMessages = new Queue<string>();
+        private ClientWebSocket _webSocket;
+        private string clientID;
+
+        public WebSocketClient() {
+            _webSocket = new ClientWebSocket();
+
+
+        }
+
+        private void init() {
+            ConnectWebSocket();
+            StartListeningForServerMessages();
+            
+        }
+
+        private static void OnMessageRecieved() {
+
+        }
+
+        private async Task StartListeningForServerMessages() {
+            while (true) {
+                byte[] buffer = new byte[1024];
+                WebSocketReceiveResult result = await _webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                string responseMessage = Encoding.ASCII.GetString(buffer, 0, result.Count);
+                responseMessages.Enqueue(responseMessage);
+                OnMessageRecieved();
+            }
+        }
+
+        private async Task ConnectWebSocket() {
+            Uri serverUri = new Uri(SERVER_URL);
+            await _webSocket.ConnectAsync(serverUri, CancellationToken.None);
+
+            clientID = await ReceiveClientID(_webSocket); // Retrieve ClientID before listening to messages to make sure that we get the right ResponseMessage
+            
+        }
+
+        private static async Task<string> ReceiveClientID(ClientWebSocket webSocket) {
+            byte[] buffer = new byte[1024];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            string clientId = Encoding.ASCII.GetString(buffer, 0, result.Count);
+            return clientId;
+        }
+
+        private async Task<string> SendAndRecieve(string communicationType, string[] data) {
+            string responseMessage = "-1"; // FAILED
+
+            // Generate a UID for the request
+            string requestId = Guid.NewGuid().ToString();
+
+            try {
+                string messageToSend = $"{requestId}:{clientID}{DELIMITER}{communicationType}";
+                foreach (string datum in data) {
+                    messageToSend += DELIMITER;
+                    messageToSend += datum;
+                }
+
+                byte[] messageBytes = Encoding.ASCII.GetBytes(messageToSend);
+                await _webSocket.SendAsync(new ArraySegment<byte>(messageBytes), WebSocketMessageType.Text, true, CancellationToken.None); // Send Request
+
+                int attempts = 0;
+                int maxAttempts = 10;
+                while(true) {
+                    attempts++;
+                    foreach (var message in responseMessages) {
+                        if (message.StartsWith(requestId)) {
+                            responseMessage = responseMessage.Substring(requestId.Length + 1);
+                            return responseMessage;
+                        }
+                    }
+                    Thread.Sleep(1000 * attempts * attempts);
+
+                    if (attempts > maxAttempts) {
+                        return responseMessage; // -1
+                    }
+                }
+                //TODO SERVER ADD REQUEST ID
+
+            } catch (Exception ex) {
+                MessageBox.Show($"Error Occurred Creating WebSocket Communication: {ex.Message}");
+            }
+            return responseMessage;
+
+        }
+
+
+
+    }
+
+
+    class WebSocketClient1 {
 
         const string DELIMITER = "|< delimiter >|"; //TODO replace with something else
         //const int PORT = 7256;
