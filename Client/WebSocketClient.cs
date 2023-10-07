36bef6d7ff5a91358e5f3233298ca30e2d92cdf2
volt2054 <ethan.williamson.2006@gmail.com>
2023-10-07 20:59:30 +0100

    removed debug

--- Client/WebSocketClient.cs
+++ Client/WebSocketClient.cs
@@ -98,10 +98,6 @@ namespace Client {
                     messageToSend += datum;
                 }
 
-                if (communicationType == TypeOfCommunication.FetchMessages) {
-                    MessageBox.Show("Sent: " + messageToSend);
-
-                }
 
 
                 byte[] messageBytes = Encoding.ASCII.GetBytes(messageToSend);
